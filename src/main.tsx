3aa6f69b7e14c7214136e2f1c6cc7032e43f38fd
gunya999-cmd <gunya999@gmail.com>
2026-05-24 23:05:03 +0300

    Use v3 app entrypoint

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import MicPreferIphoneApp from './MicPreferIphoneApp';
+import AppV3 from './AppV3';
 import './styles.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <MicPreferIphoneApp />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(React.createElement(AppV3));
