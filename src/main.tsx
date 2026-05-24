e24fa5361cab49323c45a144096f1086bb2da431
gunya999-cmd <gunya999@gmail.com>
2026-05-24 21:40:38 +0300

    Use Chrome clean app entrypoint

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import ChromeCleanApp from './ChromeCleanApp';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ChromeCleanApp />
   </React.StrictMode>
 );
