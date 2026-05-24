a1ba898846e752328dc4f40a5326911a3f290fcf
gunya999-cmd <gunya999@gmail.com>
2026-05-25 01:24:53 +0300

    Enable sync segment wrapper

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppV3 from './AppV3';
+import SyncApp from './SyncApp';
 import './styles.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(React.createElement(AppV3));
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(React.createElement(SyncApp));
